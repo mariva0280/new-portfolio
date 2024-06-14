373e4488491c69fde3436a9580a4ef84031e28ea
mariva0280 <marivafp@gmail.com>
2024-06-14 16:11:29 +0200

    Change env

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
-import 'dotenv/config'
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.API_URL
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 export const createContact = async (contact) => {
