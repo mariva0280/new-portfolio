69cf0817ea6820e130278d103e1fe16b2c8d5962
mariva0280 <marivafp@gmail.com>
2024-06-15 10:02:17 +0200

    Cors and vercel

--- backend/server.mjs
+++ backend/server.mjs
@@ -5,7 +5,9 @@ import router from './routes/contactsRoutes.mjs'
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://vanessa-fernandez-portfolio.vercel.app'
+}))
 
 app.use(express.json())
 
