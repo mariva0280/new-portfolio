34be7f15c12d80f3c3e0824b687746c0741f8b0d
mariva0280 <marivafp@gmail.com>
2024-06-18 18:53:49 +0200

    Changes in Projects section

--- src/sections/Projects/ProjectCard.jsx
+++ src/sections/Projects/ProjectCard.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import styles from './ProjectsStyles.module.css'
+import PropTypes from 'prop-types'
 
-function ProjectCard({ src, link, h3, p }) {
+function ProjectCard({ src, link, title, shortDescription, summary, isEven }){
     return (
-        <a href={link} target='_blank'>
-        <img className='hover' src={src} alt={`${h3} logo`} />
-        <h3>{h3}</h3>
-        <p>{p}</p>
-    </a>
+        <div className={`${styles.projectCard} ${isEven ? styles.even : styles.odd}`}>
+            <a href={link} target='_blank' className={styles.imageWrapper}>
+                <img className={`${styles.projectImage} hover`} src={src} alt={`${title} logo`}/>
+            </a>
+            <div className={styles.projectDetails}>
+                <h3 className={styles.projectTitle}>{title}</h3>
+                <p className={styles.shortDescription}>{shortDescription}</p>
+                <p className={styles.summary}>{summary}</p>
+            </div>
+        </div>        
     )
 }
+
+ProjectCard.propTypes = {
+    src: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    shortDescription: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    isEven: PropTypes.bool.isRequired,
+
+}
+
+
 export default ProjectCard
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,5 @@
 import styles from './ProjectsStyles.module.css'
-import hospital from '../../assets/hospital.png'
-import car from '../../assets/concesionario.png'
-import game from '../../assets/juego.png'
-import guess from '../../assets/invitados.png'
-import api from '../../assets/openapi.png'
+import projectData from './ProjectData'
 import ProjectCard from './ProjectCard'
 
 function Projects() {    
@@ -11,40 +7,17 @@ function Projects() {
     <section id="projects" className={styles.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer}>
-            <ProjectCard 
-                src={hospital} 
-                link='https://github.com/mariva0280/CitasHospital'
-                h3='Hospital'
-                p='Gestión citas hospital'    
-            />
-
-            <ProjectCard 
-                src={car} 
-                link='https://github.com/mariva0280/vehiculos'
-                h3='Concesionario'
-                p='Gestión concesionario'    
-            />
-
-            <ProjectCard 
-                src={api} 
-                link='https://github.com/mariva0280/openapiportfolio'
-                h3='Open Api'
-                p='Diseño Open Api'  
-            />
-
-            <ProjectCard 
-                src={guess} 
-                link='https://github.com/mariva0280/Challenge'
-                h3='Invitados'
-                p='Gestión invitados'
-            />
-
-            <ProjectCard 
-                src={game} 
-                link='https://github.com/mariva0280/juegoArkanoid'
-                h3='Juego'
-                p='App juego (en construcción)'  
-            />
+            {projectData.map((project, index) => (
+                <ProjectCard
+                    key={project.id}
+                    src={project.image}
+                    link={project.githubLink}
+                    title={project.title}
+                    shortDescription={project.shortDescription}
+                    summary={project.summary}
+                    isEven={index % 2 === 0}
+                />    
+            ))}    
         </div>
     </section>
   )
