57d03b1303df214383e9be3e84e4d23d28d11727
mariva0280 <marivafp@gmail.com>
2024-07-07 11:53:10 +0200

    new modification in backend

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
 import express from 'express'
 import cors from 'cors'
 import router from './routes/contactsRoutes.mjs'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
