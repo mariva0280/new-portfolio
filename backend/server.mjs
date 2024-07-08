9e5371bcb0776cfdfa39cbd030f9659bdb150e38
mariva0280 <marivafp@gmail.com>
2024-07-08 11:45:28 +0200

    Changes on server.mjs

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,10 +1,6 @@
-/* eslint-disable no-undef */
 import express from 'express'
 import cors from 'cors'
 import router from './routes/contactsRoutes.mjs'
-//import dotenv from 'dotenv'
-
-//dotenv.config()
 
 const app = express()
 // eslint-disable-next-line no-undef
